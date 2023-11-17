2f93d8ce5999f0fe543659756b2af2cb10dad267
Joilice91 <126788363+Joilice91@users.noreply.github.com>
2023-11-17 14:42:14 +0100

    update

--- resources/views/backend/postcategory/index.blade.php
+++ resources/views/backend/postcategory/index.blade.php
@@ -8,6 +8,7 @@
             @include('backend.layouts.notification')
          </div>
      </div>
+     
     <div class="card-header py-3">
       <h6 class="m-0 font-weight-bold text-primary float-left">Post Category Lists</h6>
       <a href="{{route('post-category.create')}}" class="btn btn-primary btn-sm float-right" data-toggle="tooltip" data-placement="bottom" title="Add User"><i class="fas fa-plus"></i> Add Post Category</a>
